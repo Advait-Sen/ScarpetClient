fce6e3657608b5bb09ece8119cf820e80e66bf34
Advait-Sen <advaitsen78@gmail.com>
2021-12-28 12:59:07 +0100

    Initial commit

---
+++ src/main/java/adsen/scarpet/client/ScarpetClient.java
@@ -0,0 +1,10 @@
+package adsen.scarpet.client;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ScarpetClient implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/adsen/scarpet/client/ScarpetClientClient.java
@@ -0,0 +1,13 @@
+package adsen.scarpet.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ScarpetClientClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
